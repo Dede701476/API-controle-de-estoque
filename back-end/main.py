061dc9d70dba018d5a66a673cd20266d1be5d9d2
Dede701476 <pabloandresm2008@gmail.com>
2025-11-14 10:40:58 -0300

    app.get de adicionar produto

--- back-end/main.py
+++ back-end/main.py
@@ -6,3 +6,8 @@ app = FastAPI(title="Gerência de Produtos")
 @app.get("/")
 def home():
     return {"mensagem": "Bem-vindo á gerencia de produtos"}
+
+@app.get("/adicionar")
+def adicionar_produtos(nome: str, categoria: str, valor: float, quantidade: int):
+    fun.adicionar_produtos(nome, categoria, valor, quantidade)
+    return{"mensagem" : "produto adicionado!"}
